13b0c73efec66c30ba8fe47b8d46dbda63d850cd
Radmir <galiullinradmir17@gmail.com>
2025-03-09 23:51:31 +0300

    multiple download

--- app/yandex_disk/views.py
+++ app/yandex_disk/views.py
@@ -1,5 +1,8 @@
 import os
+import io
 import requests
+import zipfile
+import json
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.http.response import JsonResponse
@@ -76,12 +79,54 @@ def list_files(request):
 @login_required
 def download_file(request):
 
-    file_url: str = request.GET.get('file_url', '')
+    if request.method == 'GET':
 
-    if file_url:
+        file_url: str = request.GET.get('file_url', '')
+        
+        if file_url:
 
-        return redirect(file_url)
-    
-    else:
+            return redirect(file_url)
+        
+        else:
 
-        return JsonResponse('file_url parameter is required')
+            return JsonResponse('file_url parameter is required')
+        
+        
+    # TODO лучше сделать временный зип(он хавает время ответа из-за ожидания закрытия fileresponse), а не хранить в памяти, генерить с u_id
+    elif request.method == 'POST':
+
+        try:
+            files = json.loads(request.POST.get('files', '[]'))  # ✅ Получаем список словарей [{file_url, file_name}]
+        
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON format'}, status=400)
+
+        if not files:
+            return JsonResponse({'error': 'No files selected'}, status=400)
+
+        # буффер для зипа
+        zip_buffer = io.BytesIO() 
+
+        with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+
+            for file in files:
+            
+                file_url = file.get("file_url")
+                file_name = file.get("file_name")
+
+                if not file_url or not file_name:
+                    continue  
+
+                response = requests.get(file_url, stream=True)
+
+                if response.status_code == 200:
+                    zip_file.writestr(file_name, response.content)
+
+
+        # буффер для зипа
+        zip_buffer.seek(0)  
+
+        response = HttpResponse(zip_buffer, content_type='application/zip')
+        response['Content-Disposition'] = 'attachment; filename="selected_files.zip"'
+
+        return response
