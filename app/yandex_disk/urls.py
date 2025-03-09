1068814fa18a22a6d938cee349896fd103ff546f
Radmir <galiullinradmir17@gmail.com>
2025-03-09 22:06:42 +0300

    once file download

--- app/yandex_disk/urls.py
+++ app/yandex_disk/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, signup
+from .views import index, signup, list_files, download_file
 
 urlpatterns = [
     path('', index, name='index'),
     path('signup/', signup, name='signup'),
-
+    path('list/', list_files, name='list_files'),
+    path('download/', download_file, name='download_file'),
 ]
--- app/yandex_disk/views.py
+++ app/yandex_disk/views.py
@@ -1,8 +1,17 @@
+import urllib
+import requests
+
+
 from django.contrib.auth import login
 from django.http import HttpResponse, HttpResponseRedirect
+from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 from .forms import SignUpForm
+
+
+YANDEX_DISK_API_BASE_URL = "https://cloud-api.yandex.net/v1/disk/public/resources"
 
 
 def index(request)  -> HttpResponse:
@@ -27,3 +36,39 @@ def signup(request) -> HttpResponseRedirect | HttpResponse:
         form = SignUpForm()
     
     return render(request, 'yandex_disk/signup.html', {'form': form})
+
+
+@login_required
+def list_files(request):
+
+    public_key = request.GET.get('public_key', '')
+
+    if not public_key:
+
+        return JsonResponse({'error': 'Public key is required'}, status=400)
+
+    params = {'public_key': public_key}
+    response = requests.get(YANDEX_DISK_API_BASE_URL, params=params)
+
+    if response.status_code == 200:
+
+        data = response.json()
+        return JsonResponse(data)
+
+    else:
+
+        return JsonResponse({'error': 'Failed to fetch files'}, status=response.status_code)
+
+
+@login_required
+def download_file(request):
+
+    file_url: str = request.GET.get('file_url', '')
+
+    if file_url:
+
+        return redirect(file_url)
+    
+    else:
+
+        return JsonResponse('file_url parameter is required')
